244d5232d58ff1e3307b3ae144e88a16eda33371
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-16 15:28:00 +0300

    add formatPlain

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,11 +1,11 @@
 const { expect, it } = require('@jest/globals');
 const { genDiff } = require('../src/withArray.cjs');
-const {comparisonNested, dfn} = require('../__fixtures__/expect.js')
+const {comparisonNested, mergeFormat} = require('../__fixtures__/expect.js')
 
 it('test genDiff format stylish', () => {
    expect(genDiff('file1.yaml', 'file2.yml', 'stylish')).toEqual(comparisonNested);
 });
 
 it('test genDiff format plain', () => {
-   expect(genDiff('file1.yaml', 'file2.yml', 'plain')).toEqual(dfn);
+   expect(genDiff('file1.yaml', 'file2.yml', 'plain')).toEqual(mergeFormat);
 });
