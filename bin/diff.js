962e34a2c49026102d4e33bc78743d923d14bccf
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-22 20:02:24 +0300

    test program

--- bin/diff.js
+++ bin/diff.js
@@ -7,6 +7,6 @@ program
   .description('Compares two configuration files and shows a difference')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  // .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => genDiff(filepath1, filepath2));
 program.parse();
