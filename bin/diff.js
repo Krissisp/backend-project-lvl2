e7e319a1915c9c77580192ee525f26ec821690e9
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-23 18:02:40 +0300

    add option

--- bin/diff.js
+++ bin/diff.js
@@ -3,11 +3,12 @@ import genDiff from '../src/gendiff';
 
 const { program } = require('commander');
 
-// .option('-f, --format <type>', 'output format', 'stylish')
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
-  .action((filepath1, filepath2) => genDiff(filepath1, filepath2));
+  .argument('<formatName>', 'display format')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, formatName) => genDiff(filepath1, filepath2, formatName));
 program.parse(process.argv);
