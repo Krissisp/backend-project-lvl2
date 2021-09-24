0b9d3d425e1ab1db900f84659f9a447229ba61ac
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-24 15:06:06 +0300

    format json

--- src/formaters/jsonFormat.js
+++ src/formaters/jsonFormat.js
@@ -6,7 +6,7 @@ const json = (array) => array.reduce((acc, element) => {
     // const key = `${element[0]} ${element[1]}`;
     acc[`${element[0]} ${element[1]}`] = element[2];
   }
-  return JSON.stringify(acc);
+  return acc;
 }, {});
 
-export default json;
+export default JSON.stringify(json());
