5fa882d0103f97daa824b4a625b53149d9526973
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-24 18:33:00 +0300

    error commander

--- gendiff.js
+++ gendiff.js
@@ -8,7 +8,6 @@ program
   .description('Compares two configuration files and shows a difference')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
-  .argument('<formatName>', 'display format')
   .option('-f --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, formatName) => genDiff(filepath1, filepath2, formatName));
 
