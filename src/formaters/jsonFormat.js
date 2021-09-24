4ddc1bdb9902aa75222ee72d7a703854ef8cdd2b
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-24 14:45:43 +0300

    format json

--- src/formaters/jsonFormat.js
+++ src/formaters/jsonFormat.js
@@ -1,10 +1,10 @@
 const json = (array) => array.reduce((acc, element) => {
   if (Array.isArray(element[2])) {
-    acc[element[0] + element[1]] = json(element[2]);
+    acc[JSON.parse(`${element[0]} ${element[1]}`)] = json(element[2]);
   }
   if (!Array.isArray(element[2])) {
     // const key = `${element[0]} ${element[1]}`;
-    acc[element[0] + element[1]] = element[2];
+    acc[JSON.parse(`${element[0]} ${element[1]}`)] = element[2];
   }
   return JSON.stringify(acc);
 }, {});
