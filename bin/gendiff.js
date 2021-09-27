6d256de55eb661ecf48e23f9d79f3144bbcc25c9
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-27 22:49:58 +0300

    add check format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
 import { genDiff } from '../src/diff.js';
 
+const program = new Command();
+
 program
-  .command('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
-  .option('-f --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, formatName) => {
+    if (formatName === undefined || formatName.format === 'stylish') {
+      console.log(genDiff(filepath1, filepath2, 'stylish'));
+    }
+    if (formatName.format === 'plain') {
+      console.log(genDiff(filepath1, filepath2, 'plain'));
+    } else {
+      console.log(genDiff(filepath1, filepath2, 'json'));
+    }
   });
 program.parse(process.argv);
