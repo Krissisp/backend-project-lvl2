12aaf753999a6b0a558b74dbe2c36a9fc468050e
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-27 17:13:25 +0300

    refactoring commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,17 +9,7 @@ program
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
   .option('-f --format <type>', 'output format', 'stylish')
-  .option('-c --colors', 'hhjkjhjk')
-  .action((filepath1, filepath2) => {
-    const option = program.opts();
-    const format = option.format;
-    if (format === 'plain') {
-      console.log(genDiff(filepath1, filepath2, 'plain'));
-    }
-    if (format === 'json') {
-      console.log(genDiff(filepath1, filepath2, 'json'));
-    } else {
-      console.log(genDiff(filepath1, filepath2, 'stylish'));
-    }
+  .action((filepath1, filepath2, format) => {
+    console.log(genDiff(filepath1, filepath2, format));
   });
 program.parse(process.argv);
