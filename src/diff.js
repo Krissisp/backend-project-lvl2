016de0360ab0e3c1dcc085f4a4d622a668cd0de5
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 13:52:57 +0300

    eslint errors

--- src/diff.js
+++ src/diff.js
@@ -31,12 +31,9 @@ export const withOutComparison = (object) => {
   }]);
   return arraySort.reduce((acc, element) => {
     if (!isObject(element[1])) {
-      acc._.concat(acc, [' ', element[0], element[1]]);
+      return _.concat(acc, [' ', element[0], element[1]]);
     }
-    if (isObject(element[1])) {
-      acc._.concat(acc, [' ', element[0], withOutComparison(element[1])]);
-    }
-    return acc;
+    return _.concat(acc, [' ', element[0], withOutComparison(element[1])]);
   }, []);
 };
 
@@ -54,40 +51,37 @@ export const createDiff = (object1, object2) => {
   return mergeObjectWithArraySort.reduce((acc, keyValue) => {
     if (!_.has(object1, `${keyValue[0]}`) && _.has(object2, `${keyValue[0]}`)) {
       if (!isObject(object2[keyValue[0]])) {
-        acc._.concat(acc, ['+', keyValue[0], object2[keyValue[0]]]);
+        return _.concat(acc, ['+', keyValue[0], object2[keyValue[0]]]);
       }
       if (isObject(object2[keyValue[0]])) {
-        acc._.concat(acc, ['+', keyValue[0], withOutComparison(object2[keyValue[0]])]);
+        return _.concat(acc, ['+', keyValue[0], withOutComparison(object2[keyValue[0]])]);
       }
     }
     if (_.has(object1, `${keyValue[0]}`) && !_.has(object2, `${keyValue[0]}`)) {
       if (!isObject(object1[keyValue[0]])) {
-        acc._.concat(acc, ['-', keyValue[0], object1[keyValue[0]]]);
+        return _.concat(acc, ['-', keyValue[0], object1[keyValue[0]]]);
       }
       if (isObject(object1[keyValue[0]])) {
-        acc._.concat(acc, ['-', keyValue[0], withOutComparison(object1[keyValue[0]])]);
+        return _.concat(acc, ['-', keyValue[0], withOutComparison(object1[keyValue[0]])]);
       }
     }
     if (_.has(object1, `${keyValue[0]}`) && _.has(object2, `${keyValue[0]}`)) {
       if (!isObject(object1[keyValue[0]]) && !isObject(object2[keyValue[0]])
       && object2[keyValue[0]] !== object1[keyValue[0]]) {
-        acc._.concat(acc, ['-', keyValue[0], object1[keyValue[0]]]);
-        acc._.concat(acc, ['+', keyValue[0], object2[keyValue[0]]]);
+        return [...acc, ['-', keyValue[0], object1[keyValue[0]]], ['+', keyValue[0], object2[keyValue[0]]]];
       }
       if (!isObject(object1[keyValue[0]]) && !isObject(object2[keyValue[0]])
       && object2[keyValue[0]] === object1[keyValue[0]]) {
-        acc._.concat(acc, [' ', keyValue[0], object1[keyValue[0]]]);
+        return _.concat(acc, [' ', keyValue[0], object1[keyValue[0]]]);
       }
       if (isObject(object1[keyValue[0]]) && !isObject(object2[keyValue[0]])) {
-        acc._.concat(acc, ['-', keyValue[0], withOutComparison(object1[keyValue[0]])]);
-        acc.push(['+', keyValue[0], object2[keyValue[0]]]);
+        return [...acc, ['-', keyValue[0], withOutComparison(object1[keyValue[0]])], ['+', keyValue[0], object2[keyValue[0]]]];
       }
       if (!isObject(object1[keyValue[0]]) && isObject(object2[keyValue[0]])) {
-        acc._.concat(acc, ['-', keyValue[0], object1[keyValue[0]]]);
-        acc._.concat(acc, ['+', keyValue[0], withOutComparison(object2[keyValue[0]])]);
+        return [...acc, ['-', keyValue[0], object1[keyValue[0]]], ['+', keyValue[0], withOutComparison(object2[keyValue[0]])]];
       }
       if (isObject(object1[keyValue[0]]) && isObject(object2[keyValue[0]])) {
-        acc._.concat(acc, [' ', keyValue[0], createDiff(object1[keyValue[0]], object2[keyValue[0]])]);
+        return _.concat(acc, [' ', keyValue[0], createDiff(object1[keyValue[0]], object2[keyValue[0]])]);
       }
     }
     return acc;
