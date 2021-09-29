4ff5572a6a41757831962ebc2ae16a689d521fd0
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 16:57:17 +0300

    refactoring stylish

--- src/formaters/stylishFormat.js
+++ src/formaters/stylishFormat.js
@@ -4,10 +4,10 @@ export default function stylish(value, replacer = ' ', space = 1) {
       const indent = element[0].length + replacer.length;
 
       if (!Array.isArray(element[2])) {
-        return `${acc}\n${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}: ${element[2]}`;
+        return `${acc}${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}: ${element[2]}`;
       }
       if (Array.isArray(element[2])) {
-        return `${acc}${iter(element[2], depth + 1)}\n${replacer.repeat(space * depth)}}`;
+        return `${acc}${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}:{ ${iter(element[2], depth + 1)}\n${replacer.repeat(space * depth)}}`;
       }
       return acc;
     }, '');
