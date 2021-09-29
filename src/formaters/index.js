b91d5187744ad7d712b894ae0454950c073e940f
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 12:43:39 +0300

    eslint errors

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -1,8 +1,8 @@
-import { plain } from './plainFormat.js';
-import { stylish } from './stylishFormat.js';
-import { json } from './jsonFormat.js';
+import plain from './plainFormat.js';
+import stylish from './stylishFormat.js';
+import json from './jsonFormat.js';
 
-export const formatChoice = (str, object) => {
+export default function formatChoice(str, object) {
   if (str === 'plain') {
     return plain(object);
   }
@@ -10,4 +10,4 @@ export const formatChoice = (str, object) => {
     return stylish(object, ' ', 4);
   }
   return JSON.stringify(json(object));
-};
+}
--- src/formaters/stylishFormat.js
+++ src/formaters/stylishFormat.js
@@ -1,4 +1,4 @@
-export const stylish = (value, replacer = ' ', space = 1) => {
+export default function stylish(value, replacer = ' ', space = 1) {
   const iter = (array, depth) => {
     const result = array.reduce((acc, element) => {
       const indent = element[0].length + replacer.length;
@@ -15,4 +15,4 @@ export const stylish = (value, replacer = ' ', space = 1) => {
     return result;
   };
   return `${iter(value, 1)}\n}`;
-};
+}
