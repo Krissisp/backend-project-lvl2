ee6c63da89577bb1efe4befbe00039cd87bcefea
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 16:50:06 +0300

    refactoring stylish

--- src/formaters/stylishFormat.js
+++ src/formaters/stylishFormat.js
@@ -2,13 +2,12 @@ export default function stylish(value, replacer = ' ', space = 1) {
   const iter = (array, depth) => {
     const result = array.reduce((acc, element) => {
       const indent = element[0].length + replacer.length;
-      acc += `\n${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}: `;
+
       if (!Array.isArray(element[2])) {
-        acc += `${element[2]}`;
+        return `${acc}\n${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}: ${element[2]}`;
       }
       if (Array.isArray(element[2])) {
-        acc += iter(element[2], depth + 1);
-        acc += `\n${replacer.repeat(space * depth)}}`;
+        return `${acc}${iter(element[2], depth + 1)}\n${replacer.repeat(space * depth)}}`;
       }
       return acc;
     }, '{');
