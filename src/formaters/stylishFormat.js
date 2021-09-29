6cd3c1129edd027e50cc115dbb3de7a38c0634c3
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 14:04:52 +0300

    eslint errors

--- src/formaters/stylishFormat.js
+++ src/formaters/stylishFormat.js
@@ -1,7 +1,7 @@
 export default function stylish(value, replacer = ' ', space = 1) {
   const iter = (array, depth) => {
     const result = array.reduce((acc, element) => {
-      const indent = 1 + replacer.length;
+      const indent = replacer.length;
       acc += `\n${replacer.repeat(space * depth - indent)}${element[0]} ${element[1]}: `;
       if (!Array.isArray(element[2])) {
         acc += `${element[2]}`;
