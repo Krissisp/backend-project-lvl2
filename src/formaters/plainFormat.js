1fce8d3249bb8581f2bca3fe691ebdfd71acb038
Kristina Palenykh <kpalenykh@bk.ru>
2021-09-29 19:42:47 +0300

    errors Assignment to function parameter "acc"

--- src/formaters/plainFormat.js
+++ src/formaters/plainFormat.js
@@ -2,9 +2,6 @@ import _ from 'lodash';
 
 export default function plain(value) {
   const iter = (array, ansentry) => {
-    array.forEach(() => {
-
-    });
     const result = array.reduce((acc, element, index) => {
       const desiredElement = element[1];
       const valueElement = element[2];
@@ -12,58 +9,55 @@ export default function plain(value) {
       const lastIndexElement1 = _.indexOf(array.flat(1), desiredElement, firstIndexElement1 + 1);
       const valueNexElement = array.flat(1)[lastIndexElement1 + 1];
 
-      if (lastIndexElement1 !== -1) {
-        acc += `\nProperty '${[...ansentry, desiredElement].join('.')}' was updated. `;
+      if (index !== 0 && desiredElement === array[index - 1][1]) {
+        return acc;
+      } if (lastIndexElement1 !== -1) {
+        const updateText = `\nProperty '${[...ansentry, desiredElement].join('.')}' was updated.`;
         if ((typeof (valueElement) === 'boolean' || typeof (valueElement) === 'number' || valueElement === null)
         && Array.isArray(valueNexElement)) {
-          acc += `From ${valueElement} to [complex value]`;
+          return `${acc}${updateText} From ${valueElement} to [complex value]`;
         }
         if ((typeof (valueElement) === 'boolean' || typeof (valueElement) === 'number' || valueElement === null)
         && !Array.isArray(valueNexElement) && typeof (valueNexElement) !== 'boolean' && valueNexElement !== null) {
-          acc += `From ${valueElement} to '${valueNexElement}'`;
+          return `${acc}${updateText} From ${valueElement} to '${valueNexElement}'`;
         }
         if ((typeof (valueElement) === 'boolean' || typeof (valueElement) === 'number' || valueElement === null)
         && (typeof (valueNexElement) === 'boolean' || valueNexElement === null || typeof (valueNexElement) === 'number')) {
-          acc += `From ${valueElement} to ${valueNexElement}`;
+          return `${acc}${updateText} From ${valueElement} to ${valueNexElement}`;
         }
         if (typeof (valueElement) !== 'boolean' && typeof (valueElement) !== 'number' && valueElement !== null && !Array.isArray(valueElement)
         && (typeof (valueNexElement) === 'boolean' || valueNexElement === null || typeof (valueNexElement) === 'number')) {
-          acc += `From '${valueElement}' to ${valueNexElement}`;
+          return `${acc}${updateText} From '${valueElement}' to ${valueNexElement}`;
         }
 
         if (Array.isArray(valueElement)
         && (typeof (valueNexElement) === 'boolean' || typeof (valueNexElement) === 'number' || valueNexElement === null)) {
-          acc += `From [complex value] to ${valueNexElement}`;
+          return `${acc}${updateText} From [complex value] to ${valueNexElement}`;
         }
         if (Array.isArray(valueElement)
         && typeof (valueNexElement) !== 'boolean' && typeof (valueNexElement) !== 'number' && valueNexElement !== null) {
-          acc += `From [complex value] to '${valueNexElement}'`;
+          return `${acc}${updateText} From [complex value] to '${valueNexElement}'`;
         }
         if (typeof (valueElement) !== 'boolean' && typeof (valueElement) !== 'number' && valueElement !== null && !Array.isArray(valueElement)
         && typeof (valueNexElement) !== 'boolean' && valueNexElement !== null && typeof (valueNexElement) !== 'number' && !Array.isArray(valueNexElement)) {
-          acc += `From '${valueElement}' to '${valueNexElement}'`;
+          return `${acc}${updateText} From '${valueElement}' to '${valueNexElement}'`;
         }
-
-        array.splice(index + 1, 1);
       } else if (element[0] === '-') {
-        acc += `\nProperty '${[...ansentry, element[1]].join('.')}' was removed`;
+        return `${acc}\nProperty '${[...ansentry, element[1]].join('.')}' was removed`;
       } else if (element[0] === '+') {
         if (Array.isArray(element[2])) {
-          acc += `\nProperty '${[...ansentry, element[1]].join('.')}' was added with value: [complex value]`;
-        } else {
-          acc += `\nProperty '${[...ansentry, element[1]].join('.')}' was added with value: `;
-          if (typeof (element[2]) === 'boolean') {
-            acc += `${element[2]}`;
-          } else {
-            acc += `'${element[2]}'`;
-          }
+          return `${acc}\nProperty '${[...ansentry, element[1]].join('.')}' was added with value: [complex value]`;
         }
+        const addedText = `\nProperty '${[...ansentry, element[1]].join('.')}' was added with value:`;
+        if (typeof (element[2]) === 'boolean') {
+          return `${acc}${addedText} ${element[2]}`;
+        }
+        return `${acc}${addedText} '${element[2]}'`;
       } else if (Array.isArray(element[2])) {
         if (element[2].length >= 2) {
-          acc += `\n${iter(element[2], [...ansentry, element[1]])}`;
-        } else {
-          acc += `\nProperty '${[...ansentry, element[1]].join('.')}' was added with value: [complex value]`;
+          return `${acc}\n${iter(element[2], [...ansentry, element[1]])}`;
         }
+        return `${acc}\nProperty '${[...ansentry, element[1]].join('.')}' was added with value: [complex value]`;
       }
       return acc;
     }, '');
